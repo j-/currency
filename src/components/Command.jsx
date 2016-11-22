f2da7ba8a01c125205506c3bdee7d3a1cdbd98ec
j- <j@skeoh.com>
2016-11-22 17:04:30 +1100

    Add a rotating placeholder to the command component

--- src/components/Command.jsx
+++ src/components/Command.jsx
@@ -4,13 +4,48 @@ import {
 	FormInput,
 } from 'elemental';
 
+const PLACEHOLDER_INTERVAL = 3000;
+
+const examples = [
+	'100 aud in usd',
+	'€50 to JPY',
+	'CNY1000 in EUR',
+];
+
+const getExampleByIndex = (index) => (
+	examples[index % examples.length]
+);
+
 export default class Command extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
 			input: '',
+			placeholder: '',
+			placeholderIndex: 0,
 		};
+		this.interval = null;
 		this.handleChangeInput = this.handleChangeInput.bind(this);
+	}
+
+	componentDidMount () {
+		this.updatePlaceholder();
+		this.interval = setInterval(
+			() => this.updatePlaceholder(),
+			PLACEHOLDER_INTERVAL
+		);
+	}
+
+	componentWillUnmount () {
+		clearInterval(this.interval);
+		this.interval = null;
+	}
+
+	updatePlaceholder () {
+		this.setState((state) => ({
+			placeholder: getExampleByIndex(state.placeholderIndex + 1),
+			placeholderIndex: state.placeholderIndex + 1,
+		}));
 	}
 
 	handleChangeInput (e) {
@@ -20,10 +55,11 @@ export default class Command extends Component {
 	}
 
 	render () {
-		const { input } = this.state;
+		const { input, placeholder } = this.state;
 		return (
 			<FormInput
 				value={ input }
+				placeholder={ placeholder }
 				onChange={ this.handleChangeInput }
 			/>
 		);
