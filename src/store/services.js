8c479655d46c99825b484347191ae0ef35b36996
j- <j@skeoh.com>
2016-11-22 15:07:08 +1100

    Add services selectors

--- src/store/services.js
+++ src/store/services.js
@@ -1,8 +1,37 @@
 import { combineReducers } from 'redux';
-import service from './service';
+import service, * as serviceModule from './service';
 
 export const SERVICE_OER = 'openexchangerates.org';
 
 export default combineReducers({
 	[SERVICE_OER]: service(SERVICE_OER),
 });
+
+const getServiceState = (state, service) => {
+	if (service in state) {
+		return state[service];
+	}
+	throw new Error(`Unrecognized service "${service}"`);
+};
+
+export const getBase = (state, service) => (
+	serviceModule.getBase(
+		getServiceState(state, service)
+	)
+);
+
+export const getRate = (state, service, code) => (
+	serviceModule.getRate(
+		getServiceState(state, service),
+		code
+	)
+);
+
+export const convert = (state, service, value, fromCode, toCode) => (
+	serviceModule.convert(
+		getServiceState(state, service),
+		value,
+		fromCode,
+		toCode
+	)
+);
