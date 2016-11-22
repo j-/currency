6b813bf372ff919d4a1ff7a22af7f421d90e9b88
j- <j@skeoh.com>
2016-11-22 23:15:42 +1100

    Add some more "parse value" tests

--- test/parse-test.js
+++ test/parse-test.js
@@ -6,5 +6,13 @@ test('parseValue function exists', (t) => {
 });
 
 test('parseValue returns correct value', (t) => {
+	t.is(parseValue('1 AUD in USD'), 1);
+	t.is(parseValue('10 AUD in USD'), 10);
 	t.is(parseValue('100 AUD in USD'), 100);
+	t.is(parseValue('33.3333 AUD in USD'), 33.3333);
+	t.is(parseValue('1,000 AUD in USD'), 1000);
+	t.is(parseValue('1,234,567.890 AUD in USD'), 1234567.89);
+	t.is(parseValue('AUD55 to USD'), 55);
+	t.is(parseValue('55AUD to USD'), 55);
+	t.is(parseValue('$55AU to USD'), 55);
 });
