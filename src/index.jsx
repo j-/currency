4948ed10a4a07e587abb923371c672648547df68
j- <j@skeoh.com>
2016-11-22 13:47:01 +1100

    Add store provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-	<div />,
+	<Provider store={ store }>
+		<div />
+	</Provider>,
 	document.getElementById('app')
 );
