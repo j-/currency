2353f46bcf3dfe5bc61f20eebe1e4a1d17dd29ec
j- <j@skeoh.com>
2016-11-22 14:02:57 +1100

    Add Redux Thunk middleware

--- src/store/prepare.js
+++ src/store/prepare.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './';
 
+const middleware = [
+	thunk,
+];
+
 export const prepareStore = (initialState = null, enhancers = compose) => {
+	// Add middleware enhancers
+	enhancers = compose(enhancers, applyMiddleware(...middleware));
+	// Add Redux devtools extension enhancer if it exists
 	if (window.__REDUX_DEVTOOLS_EXTENSION__) {
 		enhancers = compose(enhancers, window.__REDUX_DEVTOOLS_EXTENSION__());
 	}
